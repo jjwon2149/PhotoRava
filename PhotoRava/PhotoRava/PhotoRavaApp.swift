8e4bf6d099e1bc9e949bb7d230177a6e99edd77a
jjwon2149 <jjwon2149@gmail.com>
2026-01-12 23:54:56 +0900

    feat: Initialize PhotoRava app

---
+++ PhotoRava/PhotoRava/PhotoRavaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRavaApp.swift
+//  PhotoRava
+//
+//  Created by 정종원 on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoRavaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
